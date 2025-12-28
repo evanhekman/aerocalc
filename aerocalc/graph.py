3cea0426d93e57e30f67c9cdcd5c5c4f7f1d1f8c
evanhekman <ehekman@umich.edu>
2025-12-27 20:59:28 -0400

    wuz hatn

--- aerocalc/graph.py
+++ aerocalc/graph.py
@@ -1,40 +1,74 @@
-from types import FunctionType
+NodeKey = str
+ConditionKey = str
 
 
 class Edge:
-    def __init__(self, name: str, valid_func: FunctionType):
-        self.name = name
-        self.valid_func = valid_func
+    def __init__(
+        self,
+        n1: NodeKey,
+        n2: NodeKey,
+        req_all_nodes: set[NodeKey],
+        req_one_node: set[NodeKey],
+        conditions: set[ConditionKey],
+    ):
+        self.neighbor1 = n1
+        self.neighbor2 = n2
+        self.req_all_nodes = req_all_nodes
+        self.req_one_node = req_one_node
+        self.conditions = conditions
 
     def valid(self, graph: "Graph") -> bool:
-        return self.valid_func(graph)
+        # TODO: hash the graph to prevent recomputation
+        if not graph.valid_conditions(self.conditions):
+            return False
+        for n in self.req_all_nodes:
+            if not graph.valid_node(n):
+                return False
+        for n in self.req_one_node:
+            if graph.valid_node(n):
+                return True
+        return True
 
 
 class Node:
-    def __init__(self, name: str, neighbors: list[tuple["Node", "Edge"]] = []):
+    def __init__(self, name: str, neighbors: list[tuple[Node, Edge]] = []):
         self.name = name
         self.neighbors = neighbors
 
 
 class Graph:
-    def __init__(self, nodes: list[Node], edges: list[Edge]):
+    def __init__(
+        self,
+        nodes: dict[NodeKey, Node],
+        edges: list[Edge],
+        conditions: set[ConditionKey] = set([]),
+        validate: bool = True,
+    ):
         self.all_nodes = nodes
         self.all_edges = edges
+        self.conditions = conditions
+        self.active_nodes = []
+        if validate:
+            total_edges = sum([len(n.neighbors) for n in self.all_nodes.values()]) / 2
+            assert total_edges == len(self.all_edges)
+
+    def set_active(self, nodes: set[Node]):
+        self.active_nodes = nodes
 
     def connect(self, node1: Node, node2: Node, edge: Edge):
         node1.neighbors.append((node2, edge))
         node2.neighbors.append((node1, edge))
 
-    def _calculate_available(self, nodes: set[Node]) -> set["Node"]:
+    def _calculate_available(self, nodes: set[NodeKey]) -> set[NodeKey]:
         next_nodes = nodes.copy()
         for node in self.all_nodes:
             if node not in nodes:
-                for neighbor, edge in node.neighbors:
+                for neighbor, edge in self.all_nodes.[node].neighbors:
                     if neighbor in nodes and edge.valid(self):
-                        next_nodes.add(node)
+                        next_nodes.add(node.name)
         return next_nodes
 
-    def calculate_available(self, nodes: set[Node]) -> set[Node]:
+    def calculate_available(self, nodes: set[NodeKey]) -> set[NodeKey]:
         current = nodes
         next = self._calculate_available(nodes)
         while len(next) > len(current):
@@ -42,4 +76,19 @@ class Graph:
             next = self._calculate_available(current)
         return next
 
-    def solve(self, node1: Node, node2: Node): ...
+    def valid_conditions(self, test_conditions: set[ConditionKey]) -> bool:
+        return all([c in self.conditions for c in test_conditions])
+
+    def valid_node(self, node: NodeKey) -> bool:
+        return node in self.active_nodes
+
+    def solve(self, node1: NodeKey, node2: NodeKey): ...
+
+
+def build_graph(nodes: dict[str, Node], edges: list[Edge]) -> "Graph":
+    g = Graph(list(nodes.values()), [])
+    for edge in edges:
+        n1 = nodes[edge.neighbor1]
+        n2 = nodes[edge.neighbor2]
+        g.connect(n1, n2, edge)
+    return g
--- example/chem.py
+++ example/chem.py
@@ -1,3 +1,93 @@
-# PV=nRT
-# PV/T=PV/T
-#
+"""
+## equations
+PV=nRT
+U=(f/2)nRT
+
+## nodes
+- P (pressure)
+- V (volume)
+- T (temperature)
+- n (number of moles)
+- R (gas constant)
+- U (internal energy)
+- f (degrees of freedom)
+
+## edges
+### edges from PV=NRT
+- PV (given n, R, T, one of PV)
+- Pn (given V, R, T, one of Pn)
+- PR (...)
+- PT (...)
+- Vn (...)
+- VR (...)
+- VT (...)
+- nR (...)
+- nT (...)
+- RT (...)
+
+### edges from U=(f/2)nRT
+- Uf (given n, R, T, one of Uf)
+- Un (given f, R, T, one of Un)
+- UR (...)
+- UT (...)
+- fn (...)
+- fR (...)
+- fT (...)
+- nR (...)
+- nT (...)
+- RT (...)
+"""
+
+from aerocalc import graph as g
+
+NODES = {
+    "P": g.Node("P"),
+    "V": g.Node("V"),
+    "n": g.Node("n"),
+    "R": g.Node("R"),
+    "T": g.Node("T"),
+    "U": g.Node("U"),
+    "f": g.Node("f"),
+}
+
+
+def always_true(g: g.Graph):
+    return
+
+
+EDGES = [
+    # PV=nRT
+    g.Edge(always_true, NODES["P"], NODES["V"]),
+    g.Edge(always_true, NODES["P"], NODES["n"]),
+    g.Edge(always_true, NODES["P"], NODES["R"]),
+    g.Edge(always_true, NODES["P"], NODES["T"]),
+    g.Edge(always_true, NODES["V"], NODES["n"]),
+    g.Edge(always_true, NODES["V"], NODES["R"]),
+    g.Edge(always_true, NODES["V"], NODES["T"]),
+    g.Edge(always_true, NODES["n"], NODES["R"]),
+    g.Edge(always_true, NODES["n"], NODES["T"]),
+    g.Edge(always_true, NODES["P"], NODES["T"]),
+    # U=(f/2)nRT
+    g.Edge(always_true, NODES["U"], NODES["f"]),
+    g.Edge(always_true, NODES["U"], NODES["n"]),
+    g.Edge(always_true, NODES["U"], NODES["R"]),
+    g.Edge(always_true, NODES["U"], NODES["T"]),
+    g.Edge(always_true, NODES["f"], NODES["n"]),
+    g.Edge(always_true, NODES["f"], NODES["R"]),
+    g.Edge(always_true, NODES["f"], NODES["T"]),
+    g.Edge(always_true, NODES["n"], NODES["R"]),
+    g.Edge(always_true, NODES["n"], NODES["T"]),
+    g.Edge(always_true, NODES["R"], NODES["T"]),
+]
+
+GRAPH = g.build_graph(NODES, EDGES)
+
+SOME_NODES = set(
+    [
+        NODES["P"],
+        NODES["V"],
+        NODES["n"],
+        NODES["T"],
+    ]
+)
+print(GRAPH.calculate_available(SOME_NODES))
